7e1c14ae91d27e95e1d163ec9736d7f301ebbb98
djstephenson7 <dan_0502@hotmail.co.uk>
2019-12-29 18:23:28 +0000

    SearchBar Testing

--- src/screens/SearchScreen.jsx
+++ src/screens/SearchScreen.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text } from 'react-native';
 import SearchBar from '../components/SearchBar';
 
 const SearchScreen = () => {
+  const [term, setTerm] = useState('');
   return (
     <View>
-      <SearchBar />
-      <Text>Search Screen</Text>
+      <SearchBar
+        term={term}
+        onTermChange={newTerm => setTerm(newTerm)}
+        onTermSubmit={() => console.log('Term was submitted')}
+      />
+      <Text>{term}</Text>
     </View>
   );
 };
